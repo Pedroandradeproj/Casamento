8dc599b56cbab7c081cb26725a8fa49055c4225a
Pedro Andrade <130566862+Pedroandradeproj@users.noreply.github.com>
2026-04-01 22:07:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ function copiarPix() {
   navigator.clipboard.writeText(input.value)
     .then(() => {
       document.getElementById("msg").innerText = "Chave copiada!";
+      setTimeout(() => {
+        document.getElementById("msg").innerText = "";
+      }, 3000);
     })
     .catch(() => {
       document.getElementById("msg").innerText = "Erro ao copiar.";
     });
+}
+
+function voltarSite() {
+  window.location.href = "https://sites.icasei.com.br/victore_lorrane/home";
 }
